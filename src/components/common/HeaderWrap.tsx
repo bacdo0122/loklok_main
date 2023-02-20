199f753a2ca14adbe1f1dfbe30bc702409274230
Bac <bacdo0122@gmail.com>
2023-02-20 23:04:18 +0700

    add

--- src/components/common/HeaderWrap.tsx
+++ src/components/common/HeaderWrap.tsx
@@ -2,7 +2,7 @@ import { Box, BoxProps, styled } from "@mui/material";
 import ComponentWrap from "./index"
 import React from 'react'
 const Body = styled(Box)<BoxProps>({
-    minHeight: 'calc(100vh - 78px - 150px)',
+    minHeight: 'calc((100vh - 78px) - 150px)',
     maxWidth: '1240px',
     minWidth: '768px',
     position: 'relative',
